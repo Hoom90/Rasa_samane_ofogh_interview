365d86a006f281c00d6985864e829fb00dcb290e
Hoom90 <mohammad.mahdavi0015@gmail.com>
2024-09-02 20:16:43 +0330

    sidebar and header initial ui added

--- layouts/components/header.vue
+++ layouts/components/header.vue
@@ -1,3 +1,23 @@
+<script setup>
+import logoComp from "@/assets/components/logo.vue"
+import rightIconComp from "@/assets/components/right.vue"
+const breadcrumb = appStore()
+</script>
 <template>
-    header
+    <div class="p-3 bg-white text-black shadow rounded-b">
+        <div class="grid items-center text-center"
+            :class="breadcrumb.getBreadcrumbs.length == 0 ? 'grid-cols-1' : 'grid-cols-3'">
+            <div v-if="breadcrumb.getBreadcrumbs.length != 0">
+                <rightIconComp width="40" color="currentColor" />
+            </div>
+            <div v-if="breadcrumb.getPageTitle">
+                {{ breadcrumb.getPageTitle }}
+            </div>
+            <div class="flex items-center"
+                :class="breadcrumb.getBreadcrumbs.length == 0 ? 'justify-center' : 'justify-end'">
+                <span v-if="breadcrumb.getBreadcrumbs.length == 0">رسا سامانه افق</span>
+                <logoComp size="40" />
+            </div>
+        </div>
+    </div>
 </template>
--- layouts/components/sidebar.vue
+++ layouts/components/sidebar.vue
@@ -1,3 +1,33 @@
+<script setup>
+import closeIconComp from "@/assets/components/close.vue"
+import leftIconComp from "@/assets/components/left.vue"
+const memory = appStore()
+const links = [
+    { title: 'صفحه اصلی', to: '/' },
+    { title: 'پست ها', to: '/posts' },
+    { title: 'پست جدید', to: '/posts/add' },
+    { title: 'آمار بازدید', to: '/visitState' },
+]
+</script>
 <template>
-    sidebar
+    <div v-if="memory.getSidebar" class="absolute top-0 right-0 bg-white text-black w-screen h-screen z-10">
+        <div class="flex justify-center items-center h-full">
+            <div class="grid gap-1 text-center">
+                <nuxtLink v-for="(item, index) in links" :key="index" :to="item.to"
+                    class="border border-blue-500 bg-white rounded p-3 px-6">
+                    {{ item.title }}
+                </nuxtLink>
+            </div>
+        </div>
+
+        <button type="button" class="absolute top-5 left-5 cursor-pointer" @click="memory.setSidebar(false)">
+            <closeIconComp :width="40" color="#000" />
+        </button>
+    </div>
+
+    <div class="absolute top-[20%] right-0">
+        <button type="button" class="bg-white rounded-l-xl" @click="memory.setSidebar(true)">
+            <leftIconComp width="40" color="#aaa" />
+        </button>
+    </div>
 </template>
