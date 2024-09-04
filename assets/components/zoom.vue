3c5d675148e8c386f1f6c9db255bd9c048953cea
Hoom90 <mohammad.mahdavi0015@gmail.com>
2024-09-04 15:49:33 +0330

    new assets added

---
+++ assets/components/zoom.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps(['width', 'color'])
+</script>
+<template>
+    <svg :width="props.width" :height="props.width" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M10 17C13.866 17 17 13.866 17 10C17 6.13401 13.866 3 10 3C6.13401 3 3 6.13401 3 10C3 13.866 6.13401 17 10 17Z"
+            :stroke="props.color" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M20.9992 21L14.9492 14.95" :stroke="props.color" stroke-width="1.5" stroke-linecap="round"
+            stroke-linejoin="round" />
+    </svg>
+</template>
