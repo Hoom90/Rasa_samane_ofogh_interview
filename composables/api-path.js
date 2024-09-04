209908376800f7cdfa2a79802c89279e4dc15287
Hoom90 <mohammad.mahdavi0015@gmail.com>
2024-09-04 15:49:57 +0330

    api book completed

--- composables/api-path.js
+++ composables/api-path.js
@@ -1,3 +1,10 @@
 export default {
-  posts: "/posts",
+  getAllPosts: "/posts",
+  addPost: "/posts/",
+  putPost: (id) => "/posts/1",
+  // putPost: (id) `/posts/${id}`,
+  deletePost: (id) => "/posts/1",
+  // deletePost: (id) => `/posts/${id}`,
+  getAllCommentsByPostId: (id) => "/comments?postId=1",
+  // getAllCommentsByPostId: (id) => `/comments?postId=${id}`,
 };
