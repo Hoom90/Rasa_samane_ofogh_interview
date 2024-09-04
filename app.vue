36b517e03bfac054a5309fbbda399d0b24ccd203
Hoom90 <mohammad.mahdavi0015@gmail.com>
2024-09-04 15:49:16 +0330

    head added

--- app.vue
+++ app.vue
@@ -1,3 +1,16 @@
+<script setup>
+useHead({
+  titleTemplate: (page) => {
+    return page
+      ? `رسا سامانه افق - ${page}`
+      : 'رسا سامانه افق'
+  },
+  htmlAttrs: {
+    lang: 'fa',
+    dir: 'rtl'
+  },
+})
+</script>
 <template>
   <nuxtLayout>
     <nuxtPage />
